6a3bac96f499bf3375a6f527e774e2872e540b8f
Brisa Rojas <rojaasbrisa@gmail.com>
2024-03-08 08:54:33 -0300

    change styles

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -120,7 +120,6 @@ const Calculator: React.FC = () => {
         flexDirection: 'column',
         justifyContent: 'flex-end',
         boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px',
-        borderRadius: 5,
       }}
       onKeyDown={handleKeyDown}
     >
